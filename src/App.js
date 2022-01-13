2c04688e6fcf69cf644d839e08272b8e10713c36
unknown <s.nateghi1368@gmail.com>
2022-01-13 15:19:29 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu  from './Component/header/menu/Menu'
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
