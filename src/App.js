edac6e9d1dc4b532bfd50d9f73a4c273b7b98fee
unknown <s.nateghi1368@gmail.com>
2022-01-20 16:41:50 +0330

    fal page complete css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React   from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Main from './Component/mainSection/main'
-
-
+import Fal from  './Component/fal/Fal'
 
 import './App.css';
 import './assets/font/Rezvan.ttf'
 
-
+const Ashaar =()=><h3>ashaar</h3>;
+const AboutUs = () => <h3>aboutUs</h3>;
 
 function App() {
   return (
     <div className="App">
-  <Main />
+  <BrowserRouter>
+
+ <Routes>
+ <Route exact path="/" element={<Main />}/>
+      <Route path="/Fal" element={<Fal />}/>
+        <Route path="/Ashaar" element={<Ashaar />}/>
+     <Route path="/AboutUs" element={<AboutUs />}/>
+ </Routes>
+
+   </BrowserRouter>
     </div>
   );
 }
--- src/Component/header/Header.js
+++ src/Component/header/Header.js
@@ -1,34 +1,23 @@
-import React ,{Fragment}  from 'react'
-import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
-import Main from '../../Component/mainSection/main'
-import Fal from '../../Component/fal/Fal'
+import React  from 'react'
+import {Link} from 'react-router-dom'
+
 import './Header.css'
 
 import mainLogo from  '../../assets/image/logo.png'
 
-const Ashaar =()=><h3>ashaar</h3>;
-const AboutUs = () => <h3>aboutUs</h3>;
+
 
 const Header=()=>{
 
    return(
 
-    <BrowserRouter>
-    <Fragment>
    <section className='menu-section'>
-    <Link to="/"> <img src={mainLogo}/></Link>
+       <Link to="/"> <img src={mainLogo}/></Link>
         <Link  to="/Fal">فال حافظ</Link>
         <Link to="/Ashaar"> گنجینه اشعار</Link>
         <Link to="/AboutUs"> درباره ما</Link>
     </section>
- <Routes>
-  y
-      <Route path="/Fal" element={<Fal />}/>
-        <Route path="/Ashaar" element={<Ashaar />}/>
-     <Route path="/AboutUs" element={<AboutUs />}/>
- </Routes>
- </Fragment>
-   </BrowserRouter>
+ 
    )
 }
 
